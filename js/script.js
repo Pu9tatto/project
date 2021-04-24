198823635b89a70c9cbb8da7307c4bbaaee2c21f
Vladislav <wwwxumuk@mail.ru>
2021-04-24 13:43:38 +0300

    Practic part1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const personalMovieDB = {
+    count: numberOfFilms,
+    movie: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
+const a = prompt('Один из последниъ просмотренных фильмов?', ''),
+    b = prompt('На сколько оцените его', ''),
+    c = prompt('Один из последниъ просмотренных фильмов?', ''),
+    d = prompt('На сколько оцените его', '')
+personalMovieDB.movie[a] = b;
+personalMovieDB.movie[c] = d;
+
+console.log(personalMovieDB);
